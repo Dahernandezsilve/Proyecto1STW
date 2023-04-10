8f8154d6d0a986c8181c5745fc5dd957ec7afcca
Diego Hernández <dahernandezsilvestre@gmail.com>
2023-04-09 19:44:27 -0600

    Featured Work estático añadido

--- src/components/Scroll/Scroll.jsx
+++ src/components/Scroll/Scroll.jsx
@@ -6,7 +6,76 @@ function HomeFeaturedLinks() {
     <section className="homeFeaturedLinks">
       <ul className="homeFeaturedLinkList">
         <li className="homeFeaturedLinkItem" data-hover-shuffle="children">
-          
+          <h3 className="homeFeaturedItemText">Pangram Pangram</h3>
+          <div className="homeFeaturedItemTitle">
+            <span>Pangram</span>
+            <div className="homeFeaturedImage">
+              <span className="homeFeaturedImageInner">
+                <img className="featuredImage" alt="Pangram" src="src/assets/pangram.png" />
+              </span>
+            </div>
+            <span>Pangram</span>
+          </div>
+          <a href="https://locomotive.ca/en/work/pangram-pangram-foundry" className="featuredLink">Read more about this project</a>
+        </li>
+        <li className="homeFeaturedLinkItem" data-hover-shuffle="children">
+          <h3 className="homeFeaturedItemText">Baillat Studio</h3>
+          <div className="homeFeaturedItemTitle">
+            <span>Baillat</span>
+            <div className="homeFeaturedImage">
+              <span className="homeFeaturedImageInner">
+                <img className="featuredImage" alt="Pangram" src="src/assets/baillat.png" />
+              </span>
+            </div>
+            <span>Studio</span>
+          </div>
+          <a href="https://locomotive.ca/en/work/baillat-studio" className="featuredLink">Read more about this project</a>
+        </li>
+        <li className="homeFeaturedLinkItem" data-hover-shuffle="children">
+          <h3 className="homeFeaturedItemText">Age of Union</h3>
+          <div className="homeFeaturedItemTitle">
+            <span>Age</span>
+            <div className="homeFeaturedImage">
+              <span className="homeFeaturedImageInner">
+                <img className="featuredImage" alt="Pangram" src="src/assets/age.png" />
+              </span>
+            </div>
+            <span>of Union</span>
+          </div>
+          <a href="https://locomotive.ca/en/work/age-of-union" className="featuredLink">Read more about this project</a>
+        </li>
+        <li className="homeFeaturedLinkItem" data-hover-shuffle="children">
+          <h3 className="homeFeaturedItemText">It&apos;s not violent</h3>
+          <div className="homeFeaturedItemTitle">
+            <span>It&apos;s not</span>
+            <div className="homeFeaturedImage">
+              <span className="homeFeaturedImageInner">
+                <img className="featuredImage" alt="Pangram" src="src/assets/its.png" />
+              </span>
+            </div>
+            <span>violent</span>
+          </div>
+          <a href="https://locomotive.ca/en/work/it-s-not-violent" className="featuredLink">Read more about this project</a>
+        </li>
+        <li className="homeFeaturedLinkItem" data-hover-shuffle="children">
+          <h3 className="homeFeaturedItemText">Haven Studios</h3>
+          <div className="homeFeaturedItemTitle">
+            <span>Haven</span>
+            <div className="homeFeaturedImage">
+              <span className="homeFeaturedImageInner">
+                <img className="featuredImage" alt="Pangram" src="src/assets/haven.png" />
+              </span>
+            </div>
+            <span>Studios</span>
+          </div>
+          <a href="https://locomotive.ca/en/work/haven-studios" className="featuredLink">Read more about this project</a>
+        </li>
+        <li className="homeFeaturedLinkItem" data-hover-shuffle="children">
+          <h3 className="homeFeaturedItemText">All Work</h3>
+          <div className="homeFeaturedItemTitle">
+            <span>All Work</span>
+          </div>
+          <a href="https://locomotive.ca/en/work" className="featuredLink">See all projects</a>
         </li>
       </ul>
     </section>
@@ -32,6 +101,7 @@ function HomeScrollContainer() {
           </div>
         </div>
         <span className="homeSummaryFooter">
+          <img className="homeSummaryFooterImage" src="src/assets/locomotive4.png" alt="caracter4" />
           Built in MTL
         </span>
       </div>
